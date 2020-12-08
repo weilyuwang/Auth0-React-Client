c8c61624b8e3d5458dec9c4d596368c9edaf915d
weilyuwang <weilyuwang@gmail.com>
2020-12-08 17:38:35 -0500

    :sparkles: loader

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
-import { NavBar, Footer } from "./components";
+import { NavBar, Footer, Loading } from "./components";
 import { Home, Profile, ExternalApi } from "./views";
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./app.css";
 
 const App = () => {
+  const { isLoading } = useAuth0();
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div id="app" className="d-flex flex-column h-100">
       <NavBar />
